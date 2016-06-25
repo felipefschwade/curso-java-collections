01fd3a46ca584cad595335974854f8f8e8c64b23
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-06-24 22:46:03 -0400

    Aula 1 - Atividade 2 - Criando uma lista

--- src/br/com/testes/TestandoListas.java
+++ src/br/com/testes/TestandoListas.java
@@ -1,5 +1,14 @@
 package br.com.testes;
 
-public class TestandoListas {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TestandoListas {
+	public static void main(String[] args) {
+		List<String> cursos = new ArrayList<>();
+		cursos.add("Laravel");
+		cursos.add("PHPUnit");
+		cursos.add("Java e JDBC");
+		System.out.println(cursos);
+	}
 }
