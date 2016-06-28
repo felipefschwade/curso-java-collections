a27c4b1b28f33a74f9aeaaa469b3849e27330857
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-06-28 00:06:40 -0400

    Aula 3 - Atividade 3 - Adicionando novas aulas ao curso através do Add

--- src/br/com/testes/TestaCurso.java
+++ src/br/com/testes/TestaCurso.java
@@ -1,5 +1,6 @@
 package br.com.testes;
 
+import br.com.classes.Aula;
 import br.com.classes.Curso;
 
 public class TestaCurso {
@@ -10,6 +11,12 @@ public class TestaCurso {
 
 		System.out.println(curso.getAulas());
 
+		curso.getAulas().add(new Aula("Aula 1", 21));
+		curso.getAulas().add(new Aula("Aula 2", 19));
+		curso.getAulas().add(new Aula("Aula 3", 10));
+		curso.getAulas().add(new Aula("Aula 4", 23));
+
+		System.out.println(curso.getAulas());
 	}
 
 }
