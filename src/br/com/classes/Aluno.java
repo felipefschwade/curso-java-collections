939f316a80cda78a317e91300fbee945fd38ac36
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-01 00:01:19 -0400

    Aula 6 - Atividade 1 - Criando a classe Aluno

---
+++ src/br/com/classes/Aluno.java
@@ -0,0 +1,16 @@
+package br.com.classes;
+
+public class Aluno {
+
+	private String nome;
+	private int matricula;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+}
